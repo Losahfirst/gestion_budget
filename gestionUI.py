86eaefec5f287893a61341d5446e7a91f778413e
KOREKOU EMMANUEL GBRA <92445623+Losahfirst@users.noreply.github.com>
2025-05-23 12:55:41 +0000

    rtk

---
+++ gestionUI.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import filedialog
+from tkinter import ttk
+from tkinter import font
+from tkinter import scrolledtext
+from tkinter import simpledialog
+from tkinter import Menu
+from tkinter import StringVar
+
+from tkinter import IntVar
+from tkinter import BooleanVar
+from tkinter import DoubleVar
+
